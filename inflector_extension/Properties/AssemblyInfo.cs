9ca20dc96adf474d617454300abbef6fc097dd1a
Brendan Erwin <brendanjerwin@gmail.com>
2012-07-02 09:05:38 -0400

    Update version to 0.0.8.

--- inflector_extension/Properties/AssemblyInfo.cs
+++ inflector_extension/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("inflector_extension")]
 [assembly: AssemblyCopyright("Copyright (c) Brendan Erwin (and contributors) 2011")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.7")]
-[assembly: AssemblyFileVersion("0.0.7")]
+[assembly: AssemblyVersion("0.0.8")]
+[assembly: AssemblyFileVersion("0.0.8")]
 
